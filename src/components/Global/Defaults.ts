4ec808f7a265551f5598b9b7154a92611a841af5
Hammaam <hudzax@gmail.com>
2025-03-29 06:32:27 +0700

    Update Defaults configuration to disable topbar notifications and enable cover image in low quality mode

--- src/components/Global/Defaults.ts
+++ src/components/Global/Defaults.ts
@@ -1,19 +1,19 @@
 const Defaults = {
-    lyrics: {
-        api: {
-            url: "https://api.spicylyrics.org",
-            /* accessToken: "KCkLu$LyXZu2s%4$1FCJ&yUpsSYhw$5W7N%DZgq#f24$b8k*1#88iD51S#e6%!" */
-        }
+  lyrics: {
+    api: {
+      url: 'https://api.spicylyrics.org',
+      /* accessToken: "KCkLu$LyXZu2s%4$1FCJ&yUpsSYhw$5W7N%DZgq#f24$b8k*1#88iD51S#e6%!" */
     },
-    lowQualityMode: false,
-    CurrentLyricsType: "None",
-    LyricsContainerExists: false,
-    SkipSpicyFont: false,
-    OldStyleFont: false,
-    SpicyLyricsVersion: "0.0.0",
-    ForceCoverImage_InLowQualityMode: false,
-    show_topbar_notifications: true,
-    lyrics_spacing: 2,
-}
+  },
+  lowQualityMode: false,
+  CurrentLyricsType: 'None',
+  LyricsContainerExists: false,
+  SkipSpicyFont: false,
+  OldStyleFont: false,
+  SpicyLyricsVersion: '0.0.0',
+  ForceCoverImage_InLowQualityMode: true,
+  show_topbar_notifications: false,
+  lyrics_spacing: 2,
+};
 
 export default Defaults;
