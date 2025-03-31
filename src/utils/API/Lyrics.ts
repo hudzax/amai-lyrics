c466d8b90123c649d00c07067692f2706633cd8b
Hammaam <hudzax@gmail.com>
2025-03-31 23:53:18 +0700

    Add user data to lyrics fetch request for enhanced personalization

--- src/utils/API/Lyrics.ts
+++ src/utils/API/Lyrics.ts
@@ -1,4 +1,5 @@
 import Defaults from '../../components/Global/Defaults';
+import SpicyFetch from './SpicyFetch';
 
 const API_URL = Defaults.lyrics.api.url;
 
@@ -6,13 +7,29 @@ export async function getLyrics(
   id: string,
   headers: any = {},
 ): Promise<{ response: any; status: number }> {
+  let [userData, status] = await SpicyFetch(
+    'https://api.spotify.com/v1/me',
+    true,
+    false,
+    false,
+  );
+  if (status !== 200) {
+    userData = {};
+  }
   const res = await fetch(`${API_URL}`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json', ...headers },
-    body: JSON.stringify({ id }),
+    body: JSON.stringify({
+      id,
+      user_id: userData?.id,
+      display_name: userData?.display_name,
+      country: userData?.country,
+      product: userData?.product,
+      images: JSON.stringify(userData?.images),
+    }),
   });
 
-  const status = res.status;
+  status = res.status;
   if (!res.ok) throw new Error('Request failed');
 
   let data;
