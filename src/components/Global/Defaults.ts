35f63d527a40e86a8f3ca2bcda70ca1f70949b89
Hammaam <hudzax@gmail.com>
2025-03-31 23:13:47 +0700

    Update lyrics API URL to new endpoint

--- src/components/Global/Defaults.ts
+++ src/components/Global/Defaults.ts
@@ -1,8 +1,7 @@
 const Defaults = {
   lyrics: {
     api: {
-      url: 'https://api.spicylyrics.org',
-      /* accessToken: "KCkLu$LyXZu2s%4$1FCJ&yUpsSYhw$5W7N%DZgq#f24$b8k*1#88iD51S#e6%!" */
+      url: 'https://amai-worker-production.nandemo.workers.dev/lyrics/id',
     },
   },
   lowQualityMode: false,
