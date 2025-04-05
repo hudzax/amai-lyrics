25d5b137377299fc83107ae7a8354777d40dcb1b
Hammaam <hudzax@gmail.com>
2025-04-06 00:22:59 +0700

    Fixes romaja regex to include numbers
    Updates the regular expression used to identify Korean romaja text to correctly handle cases where numbers are included within the parentheses.

--- src/utils/Lyrics/Applyer/Static.ts
+++ src/utils/Lyrics/Applyer/Static.ts
@@ -64,7 +64,7 @@ export function ApplyStaticLyrics(data) {
     } else {
       // Generate ruby text for korean romaja
       line.Text = line.Text?.replace(
-        /((?:\([\uAC00-\uD7AF\u1100-\u11FF]+\)|[\uAC00-\uD7AF\u1100-\u11FF]+)[?.!,"']?){([^\}]+)}/g,
+        /((?:\([0-9\uAC00-\uD7AF\u1100-\u11FF]+\)|[\uAC00-\uD7AF\u1100-\u11FF]+)[?.!,"']?){([^\}]+)}/g,
         '<ruby class="romaja">$1<rt>$2</rt></ruby>',
       );
     }
