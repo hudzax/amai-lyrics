b960e9e9f782fb8d9f12ba7446b3f38433aa5d01
Hammaam <hudzax@gmail.com>
2025-06-04 17:16:14 +0700

    Improves type safety for header image data

--- src/components/DynamicBG/ArtistVisuals/HeaderImage/GetHeaderUrl.ts
+++ src/components/DynamicBG/ArtistVisuals/HeaderImage/GetHeaderUrl.ts
@@ -1,6 +1,16 @@
-export default function GetHeaderUrl(data: any) {
-    if (!data) return Spicetify.Player.data?.item.metadata.image_xlarge_url || null;
-    const HeaderImage = typeof data === "object" ? data?.Visuals?.headerImage?.sources[0]?.url : JSON.parse(data)?.Visuals?.headerImage?.sources[0]?.url;
-    if (!HeaderImage) return Spicetify.Player.data?.item.metadata.image_xlarge_url || null;
-    return `spotify:image:${HeaderImage.replace("https://i.scdn.co/image/", "")}`;
+type HeaderSource = { url: string };
+type HeaderImage = { sources: HeaderSource[] };
+type Visuals = { headerImage?: HeaderImage };
+type DataType = { Visuals?: Visuals };
+
+export default function GetHeaderUrl(
+  data: DataType | string | undefined,
+): string | null {
+  if (!data)
+    return Spicetify.Player.data?.item.metadata.image_xlarge_url || null;
+  const obj: DataType = typeof data === 'object' ? data : JSON.parse(data);
+  const HeaderImage = obj?.Visuals?.headerImage?.sources[0]?.url;
+  if (!HeaderImage)
+    return Spicetify.Player.data?.item.metadata.image_xlarge_url || null;
+  return `spotify:image:${HeaderImage.replace('https://i.scdn.co/image/', '')}`;
 }
